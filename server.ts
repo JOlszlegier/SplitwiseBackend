5c0fd63f31d0196a15b0edb73bfd2d81d1644806
kolszlegier41 <kolszlegier41@gmail.com>
2021-09-17 12:43:47 +0200

    fix(main): Added verifyToken middleware

--- server.ts
+++ server.ts
@@ -57,6 +57,26 @@ app.post('/login',async (req:express.Request,res:express.Response) => {
 
 })
 
+
+app.get('/add-group',verifyToken,(req,res)=>{
+    res.status(200).send({text:`Success`});
+});
+
+function verifyToken(req,res,next){
+    if(!req.headers.authorization){
+        return res.status(401).send(`No token`)
+    }
+    let token = req.headers.authorization.split(' ')[1];
+    if(token === 'null'){
+        return res.status(401).send(`Token empty`)
+    }
+    let payload = jwt.verify(token,ACCESS_TOKEN_SECRET);
+    if(!payload){
+        return res.status(401).send(`Tokens do not match!`)
+    }
+    next();
+}
+
 mongoose.connect("mongodb+srv://first_user:admin@cluster0.qzot6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     ,()=>{
         console.log('Connected to database')
