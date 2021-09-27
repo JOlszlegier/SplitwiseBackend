a81d67b25d4d5d3016eafed2023e4a6585c2e318
kolszlegier41 <kolszlegier41@gmail.com>
2021-09-27 12:29:23 +0200

    feat(main):currencyMultiplier replaced with auto scaling the value basing on exchange rate

--- model/expense.ts
+++ model/expense.ts
@@ -3,7 +3,6 @@ import * as mongoose from 'mongoose';
 const Expense = new mongoose.Schema({
     to:String,
     eachUserExpense:[{from:String,value:Number}],
-    currencyMultiplier:Number,
     description:String
 })
 
