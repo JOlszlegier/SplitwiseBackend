64155ab43c10be545fec5b7d7f866ca15176d670
kolszlegier41 <kolszlegier41@gmail.com>
2021-11-02 15:30:11 +0100

    refactor(add-friend): expense routes

---
+++ model/routes/expense-routes.ts
@@ -0,0 +1,88 @@
+import * as express from 'express';
+import {expensesFromUserGroupMode,expensesFromUserRecentMode,expensesFromUserInfoNormalMode,
+        expensesToUserInfoGroup,expensesToUserInfoRecent,expensesToUserInfoNormalMode,usersEmailsToId} from "../helpers/expense-functions";
+import {settleUpInfoGroupMode,settleUpInfoNormalMode,settleUpNormalMode,settleUpInGroup} from "../helpers/settle-up-functions";
+const User  = require("../user.ts");
+const Group = require("../group");
+const Friends = require("../friends");
+const router = express.Router();
+
+router.post('/add-expense',async (req:express.Request,res:express.Response)=>{
+    const body=req.body;
+    let usersId = [];
+    let totalAmount = 0;
+    let currentDate = new Date();
+    currentDate.setHours(currentDate.getHours() + 2)
+    if(body.groupName === 'Dashboard' || body.groupName === 'Recent Activities' || body.groupName === 'All Expenses'){
+        body.groupName = '';
+    }
+    let userArray = [];
+    for(const user of body.eachUserExpense){
+        userArray.push(user.from)
+    }
+    await usersEmailsToId(userArray,usersId,totalAmount,currentDate,req,res);
+})
+
+router.post('/balance-check',async (req:express.Request,res:express.Response)=>{
+    const body =req.body;
+    User.findOne({_id:body.userId},(error,user)=>{
+        res.send({income:user.income,outcome:user.outcome})
+    })
+})
+
+
+
+router.post('/settle-up-info',async (req:express.Request,res:express.Response)=> {
+    const body=req.body;
+    let thisUserOwes=[]
+    let finalResponseArray = [];
+    let insideExpensesId = []
+    let userNames = [];
+    let holder= {};
+    if(body.groupName === 'Dashboard' || body.groupName === 'All Expenses' || body.groupName === 'Recent Activities'){
+        settleUpInfoNormalMode(req,res,holder,thisUserOwes,finalResponseArray,userNames,insideExpensesId);
+    }else{
+        settleUpInfoGroupMode(req,res,holder,thisUserOwes,finalResponseArray,userNames,insideExpensesId);
+    }
+
+})
+
+router.post('/settle-up',async (req:express.Request,res:express.Response)=>{
+    const body = req.body;
+    const expensesToDeleteId = [];
+    let amountUserAfterFork = 0;
+    //without group
+    if(body.groupName === 'Dashboard' || body.groupName === 'All Expenses' || body.groupName === 'Recent Activities'){
+        settleUpNormalMode(req,res,expensesToDeleteId);
+    }else{ //with group
+        settleUpInGroup(req,res,expensesToDeleteId,amountUserAfterFork);
+    }
+
+})
+
+router.post('/expenses-info-to-user',async (req:express.Request,res:express.Response)=>{
+    const body= req.body;
+    const expensesUserIsOwed:[{description:String,amount:number}] = [{description:'',amount:0}];
+    if(body.groupName === 'Dashboard' || body.groupName === 'All Expenses'){
+        expensesToUserInfoNormalMode(req,res,expensesUserIsOwed);
+    }else if(body.groupName === 'Recent Activities'){
+        expensesToUserInfoRecent(req,res,expensesUserIsOwed);
+    }else{
+        expensesToUserInfoGroup(req,res,expensesUserIsOwed)
+    }
+})
+
+router.post('/expenses-info-from-user',async (req:express.Request,res:express.Response)=>{
+    const body= req.body;
+    const expensesUserIsOwing:[{description:String,amount:number}] = [{description:'',amount:0}];
+    if(body.groupName === 'Dashboard'  || body.groupName === 'All Expenses'){
+        expensesFromUserInfoNormalMode(req,res,expensesUserIsOwing);
+    }else if(body.groupName === 'Recent Activities'){
+        expensesFromUserRecentMode(req,res,expensesUserIsOwing);
+    }
+    else{
+        expensesFromUserGroupMode(req,res,expensesUserIsOwing);
+    }
+})
+
+module.exports = router
--- server.ts
+++ server.ts
@@ -36,6 +36,7 @@ const loginRouter = require("./model/routes/login-route.ts");
 const registerRouter = require("./model/routes/register-route.ts");
 const groupRouter = require("./model/routes/group-routes.ts");
 const friendRouter = require("./model/routes/friend-route.ts");
+const expenseRouter = require("./model/routes/expense-routes.ts");
 const ACCESS_TOKEN_SECRET ='7b32dcf047c86f0c6aab76639f9c99f980877a6896f5e62a9d997f6d898ffa0f0a423ac9f6b12db31d89b6e51448107d93ff95ff76011f07bf274302c86b85b2'
 
 app.use(cors({
@@ -48,87 +49,7 @@ app.use("",loginRouter);
 app.use("",registerRouter);
 app.use("",groupRouter);
 app.use("",friendRouter);
-
-
-
-
-app.post('/add-expense',async (req:express.Request,res:express.Response)=>{
-    const body=req.body;
-    let usersId = [];
-    let totalAmount = 0;
-    let currentDate = new Date();
-    currentDate.setHours(currentDate.getHours() + 2)
-    if(body.groupName === 'Dashboard' || body.groupName === 'Recent Activities' || body.groupName === 'All Expenses'){
-        body.groupName = '';
-    }
-    let userArray = [];
-    for(const user of body.eachUserExpense){
-        userArray.push(user.from)
-    }
-    await usersEmailsToId(userArray,usersId,totalAmount,currentDate,req,res);
-})
-
-app.post('/balance-check',async (req:express.Request,res:express.Response)=>{
-    const body =req.body;
-    User.findOne({_id:body.userId},(error,user)=>{
-        res.send({income:user.income,outcome:user.outcome})
-    })
-})
-
-
-
-app.post('/settle-up-info',async (req:express.Request,res:express.Response)=> {
-    const body=req.body;
-    let thisUserOwes=[]
-    let finalResponseArray = [];
-    let insideExpensesId = []
-    let userNames = [];
-    let holder= {};
-    if(body.groupName === 'Dashboard' || body.groupName === 'All Expenses' || body.groupName === 'Recent Activities'){
-        settleUpInfoNormalMode(req,res,holder,thisUserOwes,finalResponseArray,userNames,insideExpensesId);
-    }else{
-        settleUpInfoGroupMode(req,res,holder,thisUserOwes,finalResponseArray,userNames,insideExpensesId);
-    }
-
-})
-
-app.post('/settle-up',async (req:express.Request,res:express.Response)=>{
-    const body = req.body;
-    const expensesToDeleteId = [];
-    let amountUserAfterFork = 0;
-    //without group
-    if(body.groupName === 'Dashboard' || body.groupName === 'All Expenses' || body.groupName === 'Recent Activities'){
-        settleUpNormalMode(req,res,expensesToDeleteId);
-    }else{ //with group
-        settleUpInGroup(req,res,expensesToDeleteId,amountUserAfterFork);
-    }
-
-})
-
-app.post('/expenses-info-to-user',async (req:express.Request,res:express.Response)=>{
-    const body= req.body;
-    const expensesUserIsOwed:[{description:String,amount:number}] = [{description:'',amount:0}];
-    if(body.groupName === 'Dashboard' || body.groupName === 'All Expenses'){
-        expensesToUserInfoNormalMode(req,res,expensesUserIsOwed);
-    }else if(body.groupName === 'Recent Activities'){
-        expensesToUserInfoRecent(req,res,expensesUserIsOwed);
-    }else{
-        expensesToUserInfoGroup(req,res,expensesUserIsOwed)
-    }
-})
-
-app.post('/expenses-info-from-user',async (req:express.Request,res:express.Response)=>{
-    const body= req.body;
-    const expensesUserIsOwing:[{description:String,amount:number}] = [{description:'',amount:0}];
-    if(body.groupName === 'Dashboard'  || body.groupName === 'All Expenses'){
-        expensesFromUserInfoNormalMode(req,res,expensesUserIsOwing);
-    }else if(body.groupName === 'Recent Activities'){
-        expensesFromUserRecentMode(req,res,expensesUserIsOwing);
-    }
-    else{
-        expensesFromUserGroupMode(req,res,expensesUserIsOwing);
-    }
-})
+app.use("",expenseRouter);
 
 mongoose.connect("mongodb+srv://newuser:admin@cluster0.hiiuc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
     ,()=>{
